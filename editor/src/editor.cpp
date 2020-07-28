5e507c1a16cfc83d07b9d8aee6fddd761c2addb6
lenin <lenin@syneforge.com>
2020-07-28 19:27:22 +0300

    add curve.hpp

--- editor/src/editor.cpp
+++ editor/src/editor.cpp
@@ -11,6 +11,16 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+
+#include "curve.hpp"
+
+//////////////////////////////////////////////////////////////////////////
+namespace ImGui
+{
+    int Curve( const char * label, const ImVec2 & size, int maxpoints, ImVec2 * points );
+    float CurveValue( float p, int maxpoints, const ImVec2 * points );
+    float CurveValueSmooth( float p, int maxpoints, const ImVec2 * points );
+};
 //////////////////////////////////////////////////////////////////////////
 float camera_scale = 1.f;
 float camera_scale_min = 0.125f;
@@ -188,6 +198,28 @@ int editor::run()
         ImGui::Checkbox( "Custom", &Custom );
 
         ImGui::Text( "Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate );
+/*
+    Example of use:
+
+    ImVec2 foo[10];
+    ...
+    foo[0].x = -1; // init data so editor knows to take it from here
+    ...
+    if (ImGui::Curve("Das editor", ImVec2(600, 200), 10, foo))
+    {
+        // curve changed
+    }
+    ...
+    float value_you_care_about = ImGui::CurveValue(0.7f, 10, foo); // calculate value at position 0.7
+*/
+
+        ImVec2 foo[10];
+        foo[0].x = -1; // init data so editor knows to take it from here
+        if( ImGui::Curve( "Das editor", ImVec2( 600, 200 ), 10, foo ) )
+        {
+            // curve changed
+        }
+
 
         ImGui::End();
 
