d8b4f5ba1f30bed24113b678c814593ec0e0e5a3
lenin <lenin@syneforge.com>
2020-07-29 19:32:02 +0300

    wip curve editor

--- editor/src/editor.cpp
+++ editor/src/editor.cpp
@@ -19,7 +19,6 @@ namespace ImGui
 {
     int Curve( const char * label, const ImVec2 & size, int maxpoints, ImVec2 * points );
     float CurveValue( float p, int maxpoints, const ImVec2 * points );
-    float CurveValueSmooth( float p, int maxpoints, const ImVec2 * points );
 };
 //////////////////////////////////////////////////////////////////////////
 float camera_scale = 1.f;
