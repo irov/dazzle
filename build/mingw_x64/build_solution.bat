84eddbcbe6865a4dd393d834a2b43c5699f673f0
irov <irov13@mail.ru>
2020-08-21 19:23:12 +0300

    wip build mingw x64

---
+++ build/mingw_x64/build_solution.bat
@@ -0,0 +1,25 @@
+@echo off
+
+if ["%~1"]==[""] (
+  @echo invalid arguments, please select configuration
+  goto end
+)
+
+set "CONFIGURATION=%1"
+set "SOLUTION_DIR=..\solutions\dazzle_mingw_x64_%CONFIGURATION%"
+
+@pushd ..
+@mkdir %SOLUTION_DIR%
+@pushd %SOLUTION_DIR%
+
+CMake -G "Ninja" "%CD%\..\.." -DCMAKE_BUILD_TYPE:STRING=%CONFIGURATION% -DCMAKE_CONFIGURATION_TYPES:STRING=%CONFIGURATION% -DDAZZLE_EDITOR_BUILD:BOOL=ON -DDAZZLE_EXAMPLE_BUILD:BOOL=ON -DDAZZLE_TESTS:BOOL=OFF -DCMAKE_TOOLCHAIN_FILE="%CD%\..\..\cmake\mingw_x64_clang_toolchains.cmake"
+
+CMake --build .\ -j 8 --config %CONFIGURATION%
+
+@popd
+@popd
+
+:end
+@echo Done
+
+@pause
